10b2a22188b217c2de6230317368d34ae9838da5
Dylan Maccora <maccora17@gmail.com>
2020-04-04 18:09:50 -0700

    Add RPN printer

---
+++ src/main/kotlin/klox/RpnPrinter.kt
@@ -0,0 +1,24 @@
+package klox
+
+class RpnPrinter : ExprVisitor<String> {
+    fun print(expr: Expr): String {
+        return expr.accept(this)
+    }
+
+    override fun visitBinary(expr: Binary): String {
+        return "${expr.left.accept(this)} ${expr.right.accept(this)} ${expr.operator.lexeme}"
+    }
+
+    override fun visitGrouping(expr: Grouping): String {
+        return expr.expr.accept(this)
+    }
+
+    override fun visitUnary(expr: Unary): String {
+        return "${expr.operator.lexeme} ${expr.expr.accept(this)}"
+    }
+
+    override fun visitLiteral(expr: Literal): String {
+        return if (expr.value == null) "Nil" else expr.value.toString()
+    }
+
+}
